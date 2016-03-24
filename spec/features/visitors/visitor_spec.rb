ee0c1a2cf40dc1b80c1b16962c9615b598921ef5
Onur Kucukkece <onurkucukkece@gmail.com>
2016-03-24 20:57:41 +0200

    first green

--- spec/features/visitors/visitor_spec.rb
+++ spec/features/visitors/visitor_spec.rb
@@ -1,7 +1,18 @@
-describe " a visitor ", type: :feature do
-  before :each do
-  end
+require 'rails_helper'
 
-  it 'makes a search with a keyword' do
+  describe ' a visitor ', type: :feature, js: true do
+    before :each do
+    end
+
+    it 'makes a search with a keyword' do
+      visit 'https://www.google.com.tr'
+      fill_in 'lst-ib', with: 'Onur Küçükkeçe'
+      script = '
+        var elem = document.getElementsByClassName("lsb");
+        elem[0].click();
+      '
+      page.execute_script(script)
+      sleep 3
+      expect(page).to have_content 'Onur Küçükkeçe'
+    end
   end
-end
