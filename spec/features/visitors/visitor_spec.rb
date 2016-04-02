c103fe7e0f775c48cdcc86e85b78bb12c0abc302
Onur Kucukkece <onurkucukkece@gmail.com>
2016-04-02 18:34:03 +0300

    changed test to deviantart

--- spec/features/visitors/visitor_spec.rb
+++ spec/features/visitors/visitor_spec.rb
@@ -6,13 +6,14 @@ require 'rails_helper'
 
     it 'makes a search with a keyword' do
       visit 'https://www.google.com.tr'
-      fill_in 'lst-ib', with: 'Onur Küçükkeçe'
+      fill_in 'lst-ib', with: 'vacancier Onur Küçükkeçe'
       script = '
         var elem = document.getElementsByClassName("lsb");
         elem[0].click();
       '
       page.execute_script(script)
-      click_link 'Onur Küçükkeçe | LinkedIn'
-      expect(page).to have_content 'Senior Web Developer - IST@ease'
+      click_link 'Vacancier (Onur Küçükkeçe) - DeviantArt'
+      sleep 0.5
+      expect(page).to have_content 'Centraal Station'
     end
   end
