5366497140d2664cbb2ebf68d06a4be865669fe8
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-08-09 23:08:43 +0300

    simple google search

--- spec/features/visitors/visitor_spec.rb
+++ spec/features/visitors/visitor_spec.rb
@@ -1,19 +1,13 @@
 require 'rails_helper'
 
-  describe ' a visitor ', type: :feature, js: true do
-    before :each do
-    end
-
-    it 'makes a search with a keyword' do
-      visit 'https://www.google.com.tr'
-      fill_in 'lst-ib', with: 'vacancier Onur Küçükkeçe'
-      script = '
-        var elem = document.getElementsByClassName("lsb");
-        elem[0].click();
-      '
-      page.execute_script(script)
-      click_link 'Vacancier (Onur Küçükkeçe) - DeviantArt'
-      sleep 0.5
-      expect(page).to have_content 'Centraal Station'
-    end
+describe ' a visitor ', type: :feature, js: true do
+  it 'makes a search with a keyword' do
+    visit 'https://www.google.com.tr'
+    find(:css, 'input[title="Ara"]').set('boostads heroku')
+    find(:css, 'button[value="Ara"]').click
+    sleep 3
+    first('a', text: /BoostAds/).click
+    sleep 3
+    expect(page).to have_content 'Welcome'
   end
+end
